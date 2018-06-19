5a0c651a253720dd6dc394a3c0b4f871811bdf77
Swen Kunzel <swenkuenzel@gmail.com>
2018-06-19 23:15:00 +0100

    methods to get leader and politicians by year which are not in prison

--- app/models/party.rb
+++ app/models/party.rb
@@ -3,44 +3,37 @@ class Party < ApplicationRecord
  has_many :subordinates
 
   def leader(year)
-    politician_leader = self.politicians.find_by(leader: 1)
-    leader_prison = Event.where("politician_id = #{politician_leader.id} AND action = 'prison' ")
-    leader_free = Event.where("politician_id = #{politician_leader.id} AND action = 'free' ")
+    if year < leader_year_prison || year > leader_year_free
+      return self.politicians.find_by(leader: 1)
+    elsif politicians_free(year).any?
+      return politicians_free(year).first
+    else
+      return self.subordinates.first
+    end
+  end
 
-    politicians = self.politicians.where(" leader = false ")
-    politician_select = []
-    politicians.each do |politician|
+  def leader_year_prison
+    politician_leader = self.politicians.find_by(leader: 1)
+    leader_prison = Event.where("politician_id = #{politician_leader.id} AND action = 'prison' ").first.year
+  end
+
+  def leader_year_free
+    politician_leader = self.politicians.find_by(leader: 1)
+    leader_free = Event.where("politician_id = #{politician_leader.id} AND action = 'free' ").first.year
+  end
+
+  def politicians_free(year)
+    politicians_selected = []
+    self.politicians.where(" leader = false ").each do |politician|
       politician_prison = Event.where("politician_id = #{politician.id} AND action = 'prison' ")
       politician_free = Event.where("politician_id = #{politician.id} AND action = 'free' ")
-      if politician_prison.exists? || politician_prison.exists?
-        if year < politician_prison.first.year || year > politician_free.first.year
-          politician_select << politician
-        end
+      if year < politician_prison.first.year || year > politician_free.first.year
+        politicians_selected << politician
       end
     end
-  if year < leader_prison.first.year || year > leader_free.first.year
-    return self.politicians.find_by(leader: 1)
-  elsif politician_select.any?
-    return politician_select.first
-  else
-    return self.subordinates.first
+    return politicians_selected
   end
 
-  end
 end
 
- #   if self.politicians.exists?(leader: 1)
-  #    self.politicians.find_by(leader: 1)
 
-   # elsif self.politicians.where('leader: 0)'.order('birthyear asc').first
-    #  self.politicians.where('leader: 0)'.order('birthyear asc').first
-
-    #else
-    #  self.subordinates.order('birthyear asc').first
-    #end
-  #end
-#end
-
-
-
-#&& Event.where("year = year AND politician_id = #{self.politicians.find_by(leader: 1).id} AND action = 'prison' ").empty? == true
